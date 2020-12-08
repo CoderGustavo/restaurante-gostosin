32f9f4982a8e26d8966a170525c9a73c8aa2f7c0
Gustavo Ornaghi Antunes <gustavoornaghiantunes@gmail.com>
2020-12-08 01:38:25 -0300

    Atualizando arquivos 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,20 @@ $(document).ready(function() {
   });
 });
 
+
+$(document).ready(function(){
+  $(".filtro").click(function(){
+     const value = $(this).attr('data-filter');
+     if(value == "todos"){
+       $('.list-card').show('1000')
+     }else{
+       $('.list-card').not('.'+value).hide('1000');
+       $('.list-card').filter('.'+value).show('1000')
+     }
+  });
+});
+
+$('.filtro').click(function(){
+  $(this).addClass("active").siblings().removeClass('active');
+
+});
